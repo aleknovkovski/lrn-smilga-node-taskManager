4fef771ea94440ecab0fc9cdb62a74fed91ece7b
Alek Novkovski <alek@greatcode2.com>
2023-01-18 00:38:02 +0100

    Add section delineation comments

--- working/app.js
+++ working/app.js
@@ -2,7 +2,11 @@ const express = require('express')
 const app = express()
 const tasks = require('./routes/tasks')
 
+// middleware
+
+// routes
 app.use('/api/v1/tasks', tasks)
+
 app.listen(5000,()=>{
     console.log("Server is listening on port 5000")
 })
