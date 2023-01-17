e192fe990bac545cc1a78d7fc24e81bc3ef6b9e2
Alek Novkovski <alek@greatcode2.com>
2023-01-18 00:20:25 +0100

    Setup route for getting all tasks

--- working/app.js
+++ working/app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const tasks = require('./routes/tasks')
 
-app.use('/api/v1/', tasks)
+app.use('/api/v1/tasks', tasks)
 app.listen(5000,()=>{
     console.log("Server is listening on port 5000")
 })
--- working/routes/tasks.js
+++ working/routes/tasks.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/hello', (req, res)=> {
-    res.send("Helooo there")
+router.get('/', (req, res)=> {
+    res.send("All items")
 })
 
 module.exports = router
