ea7c73ed69f6acc0535ed220e80f5639bdea8029
Alek Novkovski <alek@greatcode2.com>
2023-01-17 23:09:56 +0100

    Setup basic express server w /hello route

--- working/app.js
+++ working/app.js
@@ -1,1 +1,9 @@
-console.log('Task Manager App')
+const express = require('express')
+const app = express()
+
+app.get('/hello', (req, res)=> {
+    res.send("Helooo there")
+})
+app.listen(5000,()=>{
+    console.log("Server is listening on port 5000")
+})
