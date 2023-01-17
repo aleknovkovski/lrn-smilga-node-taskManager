121b407e864cf31ecd556ba3b42d10bc3f81cabc
Alek Novkovski <alek@greatcode2.com>
2023-01-18 00:44:01 +0100

    Move getAllTasks logic to controllers file

---
+++ working/controllers/tasks.js
@@ -0,0 +1,5 @@
+function getAllTasks(req, res) {
+    res.send("All items")
+}
+
+module.exports = {getAllTasks}
--- working/routes/tasks.js
+++ working/routes/tasks.js
@@ -1,8 +1,7 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/', (req, res)=> {
-    res.send("All items")
-})
+const {getAllTasks} = require('../controllers/tasks')
+router.get('/', getAllTasks)
 
 module.exports = router
