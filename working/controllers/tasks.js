a87e610b1ee6404e7683add85a0682225f0425af
Alek Novkovski <alek@greatcode2.com>
2023-01-18 01:02:29 +0100

    Setup all tasks routes

--- working/controllers/tasks.js
+++ working/controllers/tasks.js
@@ -2,4 +2,25 @@ function getAllTasks(req, res) {
     res.send("All items")
 }
 
-module.exports = {getAllTasks}
+function createTask(req, res) {
+    res.send("Create task")
+}
+
+function getTask(req, res) {
+    res.send("Get task")
+}
+
+function updateTask(req, res) {
+    res.send("Update task")
+}
+
+function deleteTask(req, res) {
+    res.send("Delete task")
+}
+module.exports = {
+    getAllTasks,
+    createTask,
+    getTask,
+    updateTask,
+    deleteTask
+}
