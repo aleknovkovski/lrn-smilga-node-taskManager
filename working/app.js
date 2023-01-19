040df245c139963f6e640437cf5b23536cb4ab25
Alek Novkovski <alek@greatcode2.com>
2023-01-19 12:40:04 +0100

    Refactor to store mongoDB connection string in dotEnv

--- working/app.js
+++ working/app.js
@@ -2,6 +2,7 @@ const connectDB = require('./db/connect')
 const express = require('express')
 const app = express()
 const tasks = require('./routes/tasks')
+require('dotenv').config()
 
 // middleware
 app.use(express.json())
@@ -11,7 +12,7 @@ app.use('/api/v1/tasks', tasks)
 
 async function start () {
         try {
-            await connectDB()
+            await connectDB(process.env.MONGO_URI)
             app.listen(5000,()=>{
                 console.log("Server is listening on port 5000")
             })
--- working/db/connect.js
+++ working/db/connect.js
@@ -1,9 +1,7 @@
 const mongoose = require('mongoose')
 
-const connectionString = 'mongodb+srv://aleknovkovski:udAHMPCDztp8zBiG@nodeexpressprojects.qvwfxqy.mongodb.net/taskManager?retryWrites=true&w=majority'
-
 function connectDB (url) {
-    return mongoose.connect(connectionString)
+    return mongoose.connect(url)
 }
 
 module.exports = connectDB
