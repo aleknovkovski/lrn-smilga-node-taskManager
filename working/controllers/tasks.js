61305f6f70c9081c311aa4acc9746f510e9fe854
Alek Novkovski <alek@greatcode2.com>
2023-01-20 15:38:44 +0100

    Refactor, wrap controllers in middleware for try-catch

--- working/controllers/tasks.js
+++ working/controllers/tasks.js
@@ -1,61 +1,38 @@
 const Task = require('../models/Task')
+const asyncWrapper = require('../middleware/async')
 
-async function getAllTasks(req, res) {
-    try{
-        const tasks = await Task.find({})
-        res.json({message: "Get all tasks", tasks: tasks})
-    }
-    catch (err){
-        res.status(500).json({"message": err})
-    }
-}
+const getAllTasks = asyncWrapper(async function (req, res) {
+    const tasks = await Task.find({})
+    res.json({message: "Get all tasks", tasks: tasks})
+})
 
-async function createTask(req, res) {
-    try {
-        const task = await Task.create(req.body)
-        res.json({message: "Create task", request: task})
-    } catch (err) {
-        res.status(500).json({"message": err})
-    }
-}
+const createTask = asyncWrapper(async function (req, res) {
+    const task = await Task.create(req.body)
+    res.json({message: "Create task", request: task})
+})
 
-async function getTask(req, res) {
-    const {id:taskId} = req.params
-    try{
-        const task = await Task.findOne({_id: taskId})
-        if (!task) {return res.status(404).json({message: "Task not found"})}
-        res.json({message: "Get task", task: task})
-    }
-    catch(err){
-        res.status(500).json({"message": err})
-    }
-}
+const getTask = asyncWrapper(async function (req, res) {
+    const {id:taskId} = req.params;
+    const task = await Task.findOne({_id: taskId})
+    if (!task) {return res.status(404).json({message: "Task not found"})}
+    res.json({message: "Get task", task: task})
+})
 
-async function updateTask(req, res) {
+const updateTask = asyncWrapper(async function (req, res) {
     const {id:taskId} = req.params;
     const query = {_id: taskId}, update = req.body,
         options = {returnDocument: "after", runValidators: true}
-    try{
-        const task = await Task.findOneAndUpdate(query, update, options)
-        if (!task) {return res.status(404).json({message: "Task not found"})}
-        res.json({message: "Update task", task: task})
-    }
-    catch(err){
-        res.status(500).json({"message": err})
-    }
-}
+    const task = await Task.findOneAndUpdate(query, update, options)
+    if (!task) {return res.status(404).json({message: "Task not found"})}
+    res.json({message: "Update task", task: task})
+})
 
-async function deleteTask(req, res) {
+const deleteTask = asyncWrapper(async function (req, res) {
     const {id:taskId} = req.params
-    try{
-        const task = await Task.findOneAndDelete({_id: taskId})
-        if (!task) {return res.status(404).json({message: "Task not found"})}
-        res.json({message: "Delete task", task: task})
-    }
-    catch(err){
-        res.status(500).json({"message": err})
-    }
-}
+    const task = await Task.findOneAndDelete({_id: taskId})
+    if (!task) {return res.status(404).json({message: "Task not found"})}
+    res.json({message: "Delete task", task: task})
+})
 
 module.exports = {
     getAllTasks,
