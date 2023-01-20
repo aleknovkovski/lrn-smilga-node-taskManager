0e5cb75a557ff247be18322c12aebbb558d4591b
Alek Novkovski <alek@greatcode2.com>
2023-01-20 20:41:07 +0100

    Refactor error handler to work with custom errors

--- working/middleware/error-handler.js
+++ working/middleware/error-handler.js
@@ -1,5 +1,10 @@
+const {CustomAPIError} = require('../errors/custom-error')
+
 function errorHandlerMiddleware(err, req, res, next) {
-    return res.status(err.status).json({message: err.message})
+    if(err instanceof CustomAPIError) {
+        return res.status(err.statusCode).json({message: err.message})
+    }
+    return res.status(500).json({message: "Something went wrong"})
 }
 
 module.exports = errorHandlerMiddleware
